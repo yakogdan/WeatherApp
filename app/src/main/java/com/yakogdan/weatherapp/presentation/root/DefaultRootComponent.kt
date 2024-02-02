c5903c07c75507ff3ae925f0eef48734522c125c
yakogdan <yakogdan@gmail.com>
2024-02-02 22:59:04 +0800

    Реализация RootComponent.

--- app/src/main/java/com/yakogdan/weatherapp/presentation/root/DefaultRootComponent.kt
+++ app/src/main/java/com/yakogdan/weatherapp/presentation/root/DefaultRootComponent.kt
@@ -1,9 +1,95 @@
 package com.yakogdan.weatherapp.presentation.root
 
+import android.os.Parcelable
 import com.arkivanov.decompose.ComponentContext
+import com.arkivanov.decompose.router.stack.ChildStack
+import com.arkivanov.decompose.value.Value
+import com.yakogdan.weatherapp.domain.entity.City
+import com.yakogdan.weatherapp.presentation.details.DefaultDetailsComponent
+import com.yakogdan.weatherapp.presentation.favourite.DefaultFavouriteComponent
+import com.yakogdan.weatherapp.presentation.search.DefaultSearchComponent
+import com.yakogdan.weatherapp.presentation.search.OpenReason
+import dagger.assisted.Assisted
+import dagger.assisted.AssistedFactory
+import dagger.assisted.AssistedInject
+import kotlinx.parcelize.Parcelize
 
-class DefaultRootComponent(
-    componentContext: ComponentContext
+class DefaultRootComponent @AssistedInject constructor(
+    private val detailsComponentFactory: DefaultDetailsComponent.Factory,
+    private val favouriteComponentFactory: DefaultFavouriteComponent.Factory,
+    private val searchComponentFactory: DefaultSearchComponent.Factory,
+    @Assisted("componentContext") componentContext: ComponentContext
 ) : RootComponent, ComponentContext by componentContext {
+    override val stack: Value<ChildStack<*, RootComponent.Child>>
+        get() = TODO("Not yet implemented")
 
+    private fun child(
+        config: Config,
+        componentContext: ComponentContext
+    ): RootComponent.Child {
+        return when (config) {
+            is Config.Details -> {
+                val component = detailsComponentFactory.create(
+                    city = config.city,
+                    onBackClicked = {
+
+                    },
+                    componentContext = componentContext
+                )
+                RootComponent.Child.Details(component)
+            }
+
+            Config.Favourite -> {
+                val component = favouriteComponentFactory.create(
+                    onCityItemClicked = {
+
+                    },
+                    onAddFavouriteClicked = {
+
+                    },
+                    onSearchClicked = {
+
+                    },
+                    componentContext = componentContext
+                )
+                RootComponent.Child.Favourite(component)
+            }
+
+            is Config.Search -> {
+                val component = searchComponentFactory.create(
+                    openReason = config.openReason,
+                    onBackClick = {
+
+                    },
+                    onCitySavedToFavourite = {
+
+                    },
+                    onForecastForCityRequest = {
+
+                    },
+                    componentContext = componentContext
+                )
+                RootComponent.Child.Search(component)
+            }
+        }
+    }
+
+    sealed interface Config : Parcelable {
+
+        @Parcelize
+        data object Favourite : Config
+
+        @Parcelize
+        data class Search(val openReason: OpenReason) : Config
+
+        @Parcelize
+        data class Details(val city: City) : Config
+    }
+
+    @AssistedFactory
+    interface Factory {
+        fun create(
+            @Assisted("componentContext") componentContext: ComponentContext
+        ): DefaultRootComponent
+    }
 }
--- app/src/main/java/com/yakogdan/weatherapp/presentation/root/RootComponent.kt
+++ app/src/main/java/com/yakogdan/weatherapp/presentation/root/RootComponent.kt
@@ -1,4 +1,21 @@
 package com.yakogdan.weatherapp.presentation.root
 
+import com.arkivanov.decompose.router.stack.ChildStack
+import com.arkivanov.decompose.value.Value
+import com.yakogdan.weatherapp.presentation.details.DetailsComponent
+import com.yakogdan.weatherapp.presentation.favourite.FavouriteComponent
+import com.yakogdan.weatherapp.presentation.search.SearchComponent
+
 interface RootComponent {
+
+    val stack: Value<ChildStack<*, Child>>
+
+    sealed interface Child {
+
+        data class Favourite(val component: FavouriteComponent) : Child
+
+        data class Search(val component: SearchComponent) : Child
+
+        data class Details(val component: DetailsComponent) : Child
+    }
 }
