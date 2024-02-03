02989d0bbe78500dad11d9b544568a776b643098
yakogdan <yakogdan@gmail.com>
2024-02-03 20:22:46 +0800

    В зависимости от стейта отрисовывается температура на айтеме. Добавлена кнопка добавление избранного города. Добавлено поле поиска города. Реализована навигация по элементам FavouriteContent.

--- app/src/main/java/com/yakogdan/weatherapp/presentation/favourite/FavouriteContent.kt
+++ app/src/main/java/com/yakogdan/weatherapp/presentation/favourite/FavouriteContent.kt
@@ -1,17 +1,31 @@
 package com.yakogdan.weatherapp.presentation.favourite
 
+import androidx.compose.foundation.BorderStroke
 import androidx.compose.foundation.background
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.sizeIn
 import androidx.compose.foundation.lazy.grid.GridCells
+import androidx.compose.foundation.lazy.grid.GridItemSpan
 import androidx.compose.foundation.lazy.grid.LazyVerticalGrid
 import androidx.compose.foundation.lazy.grid.itemsIndexed
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Edit
+import androidx.compose.material.icons.filled.Search
 import androidx.compose.material3.Card
 import androidx.compose.material3.CardDefaults
+import androidx.compose.material3.CircularProgressIndicator
+import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
@@ -23,9 +37,16 @@ import androidx.compose.ui.draw.drawBehind
 import androidx.compose.ui.draw.shadow
 import androidx.compose.ui.geometry.Offset
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import com.bumptech.glide.integration.compose.ExperimentalGlideComposeApi
+import com.bumptech.glide.integration.compose.GlideImage
+import com.yakogdan.weatherapp.R
+import com.yakogdan.weatherapp.presentation.extensions.tempToFormattedString
 import com.yakogdan.weatherapp.presentation.ui.CardGradients
 import com.yakogdan.weatherapp.presentation.ui.Gradient
+import com.yakogdan.weatherapp.presentation.ui.theme.Orange
 
 @Composable
 fun FavouriteContent(component: FavouriteComponent) {
@@ -39,22 +60,39 @@ fun FavouriteContent(component: FavouriteComponent) {
         verticalArrangement = Arrangement.spacedBy(24.dp),
         horizontalArrangement = Arrangement.spacedBy(16.dp)
     ) {
+        item(span = { GridItemSpan(2) }) {
+            SearchCard(
+                onClick = { component.onClickSearch() }
+            )
+        }
         itemsIndexed(
             items = state.cityItems,
             key = { _, item -> item.city.id }
         ) { index, item ->
             CityCard(
                 cityItem = item,
-                index = index
+                index = index,
+                onClick = {
+                    component.onCityItemClick(item.city)
+                }
+            )
+        }
+        item {
+            AddFavouriteCityCard(
+                onClick = {
+                    component.onClickAddFavourite()
+                }
             )
         }
     }
 }
 
+@OptIn(ExperimentalGlideComposeApi::class)
 @Composable
 private fun CityCard(
     cityItem: FavouriteStore.State.CityItem,
-    index: Int
+    index: Int,
+    onClick: () -> Unit
 ) {
     val gradient = getGradientByIndex(index)
     val shape = MaterialTheme.shapes.extraLarge
@@ -71,6 +109,7 @@ private fun CityCard(
     ) {
         Box(
             modifier = Modifier
+                .clickable { onClick() }
                 .background(gradient.primaryGradient)
                 .fillMaxSize()
                 .sizeIn(minHeight = 196.dp)
@@ -86,11 +125,108 @@ private fun CityCard(
                 }
                 .padding(24.dp)
         ) {
+            when (val weatherState = cityItem.weatherState) {
+
+                FavouriteStore.State.WeatherState.Error -> {}
+
+                FavouriteStore.State.WeatherState.Initial -> {}
+
+                is FavouriteStore.State.WeatherState.Loaded -> {
+                    GlideImage(
+                        modifier = Modifier
+                            .align(Alignment.TopEnd)
+                            .size(56.dp),
+                        contentDescription = null,
+                        model = weatherState.iconUrl
+                    )
+                    Text(
+                        modifier = Modifier
+                            .align(Alignment.BottomStart)
+                            .padding(bottom = 24.dp),
+                        color = MaterialTheme.colorScheme.background,
+                        style = MaterialTheme.typography.bodyLarge.copy(fontSize = 48.sp),
+                        text = weatherState.tempC.tempToFormattedString()
+                    )
+                }
+
+                FavouriteStore.State.WeatherState.Loading -> {
+                    CircularProgressIndicator(
+                        modifier = Modifier.align(Alignment.Center),
+                        color = MaterialTheme.colorScheme.background
+                    )
+                }
+            }
+
             Text(
                 modifier = Modifier.align(Alignment.BottomStart),
-                text = cityItem.city.name,
                 style = MaterialTheme.typography.titleMedium,
-                color = MaterialTheme.colorScheme.background
+                color = MaterialTheme.colorScheme.background,
+                text = cityItem.city.name
+            )
+        }
+    }
+}
+
+@Composable
+private fun AddFavouriteCityCard(
+    onClick: () -> Unit
+) {
+    Card(
+        colors = CardDefaults.cardColors(containerColor = Color.Transparent),
+        shape = MaterialTheme.shapes.extraLarge,
+        border = BorderStroke(1.dp, MaterialTheme.colorScheme.onBackground)
+    ) {
+        Column(
+            modifier = Modifier
+                .clickable { onClick() }
+                .sizeIn(minHeight = 196.dp)
+                .fillMaxWidth()
+                .padding(24.dp)
+        ) {
+            Icon(
+                modifier = Modifier
+                    .align(Alignment.CenterHorizontally)
+                    .padding(16.dp)
+                    .size(48.dp),
+                tint = Orange,
+                contentDescription = null,
+                imageVector = Icons.Default.Edit
+            )
+            Spacer(modifier = Modifier.weight(1f))
+            Text(
+                modifier = Modifier.align(Alignment.CenterHorizontally),
+                style = MaterialTheme.typography.titleMedium,
+                text = stringResource(R.string.button_add_favourite)
+            )
+        }
+    }
+}
+
+@Composable
+private fun SearchCard(
+    onClick: () -> Unit
+) {
+    val gradient = CardGradients.gradients[3]
+    Card(
+        shape = CircleShape
+    ) {
+        Row(
+            modifier = Modifier
+                .clickable { onClick() }
+                .fillMaxWidth()
+                .background(gradient.primaryGradient),
+            verticalAlignment = Alignment.CenterVertically
+        ) {
+            Icon(
+                modifier = Modifier.padding(vertical = 8.dp, horizontal = 16.dp),
+                tint = MaterialTheme.colorScheme.background,
+                contentDescription = null,
+                imageVector = Icons.Default.Search
+            )
+            Text(
+                modifier = Modifier.padding(end = 16.dp),
+                color = MaterialTheme.colorScheme.background,
+                text = stringResource(R.string.search)
             )
         }
     }
